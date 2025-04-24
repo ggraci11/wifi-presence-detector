a6ce8551019cc63d948e9fca40dcb59db55ab3aa
ggraci11 <gdgracia2279@gmail.com>
2025-04-24 09:27:06 -0500

    Recieves port that Render Service provides

--- server.py
+++ server.py
@@ -57,4 +57,5 @@ def upload():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
